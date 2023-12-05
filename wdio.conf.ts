2212d531f8181df7ccf06890369546fd0fe71bfd
Saurabh <Saurabh.verma@bitfinex.com>
2023-12-05 14:49:10 +0100

    fixeD

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -94,7 +94,7 @@ import type { Options } from '@wdio/types'
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: 'http://localhost',
+    //baseUrl: 'http://localhost',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
